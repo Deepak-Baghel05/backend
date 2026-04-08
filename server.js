10dc1e4877c704eacb469672e0d7cc1ed83213fe
Deepak-Baghel05 <deepakkbaghel0111@gmail.com>
2026-04-08 11:04:02 +0530

    Authentication version

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const db = require('./db');
 const user = require("./models/user");
 const project = require("./models/project");
 const task = require("./models/task");
-const dotenv = require('dotenv');
+require('dotenv').config();
+const passport = require('./auth'); 
 const app = express();
 
+const logRequest = (req,res,next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Mode to : ${req.method} ${req.url}`);
+    next();
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware  = passport.authenticate("local",{session:false});
 app.get("/", (req,res) => {
     res.send("Hello team task manager.")
 })
